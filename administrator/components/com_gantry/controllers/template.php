8f68ea70fabbfd80ade6ef925765c15081636944
Pablo Arias <>
2013-06-11 02:36:45 +0200

    Actualizado framework Gantry a la versión 4.1.10.

--- administrator/components/com_gantry/controllers/template.php
+++ administrator/components/com_gantry/controllers/template.php
@@ -2,7 +2,7 @@
 /**
  * @package   gantry
  * @subpackage core
- * @version   4.1.9 April 1, 2013
+ * @version   4.1.10 May 7, 2013
  * @author    RocketTheme http://www.rockettheme.com
  * @copyright Copyright (C) 2007 - 2013 RocketTheme, LLC
  * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 only
