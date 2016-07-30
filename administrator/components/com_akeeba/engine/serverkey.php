53a7c84207f77e416ba5ff199f00de0f80b15346
Pablo Arias <correo@pabloarias.eu>
2016-07-30 13:10:42 +0200

    Removed older version of Joomla to start clean with J 3.6.

--- administrator/components/com_akeeba/engine/serverkey.php
+++
@@ -1,1 +0,0 @@
-<?php defined('AKEEBAENGINE') or die(); define('AKEEBA_SERVERKEY', 'MDExZTlhMTQ1NzdhODI4M2JiZTk4NGU0OWMxMDZjMzQ='); ?>
--- administrator/components/com_akeeba/version.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Protect from unauthorized access
-defined('_JEXEC') or die('Restricted Access');
-
-define('AKEEBA_PRO', '0');
-define('AKEEBA_VERSION', '4.5.1');
-define('AKEEBA_DATE', '2015-12-22');
