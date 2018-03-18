be370925923eb1fbb0c7e6661dde665f7361459d
Pablo Arias <correo@pabloarias.eu>
2018-03-18 10:52:29 +0100

    Updated JCE

--- components/com_jce/editor/tiny_mce/plugins/browser/classes/browser.php
+++ components/com_jce/editor/tiny_mce/plugins/browser/classes/browser.php
@@ -17,7 +17,7 @@ class WFFileBrowserPlugin extends WFMediaManager
     /*
      * @var string
      */
-    protected $_filetypes = 'doc,docx,ppt,pptx,xls,xlsx,gif,jpeg,jpg,png,pdf,zip,tar,gz,swf,rar,mov,mp4,qt,wmv,asx,asf,avi,wav,mp3,aiff,odt,odg,odp,ods,odf,rtf,txt,csv';
+    protected $_filetypes = 'doc,docx,ppt,pps,pptx,ppsx,xls,xlsx,gif,jpeg,jpg,png,pdf,zip,tar,gz,swf,rar,mov,mp4,qt,wmv,asx,asf,avi,wav,mp3,aiff,odt,odg,odp,ods,odf,rtf,txt,csv';
 
     public function __construct($config = array())
     {
@@ -31,22 +31,32 @@ class WFFileBrowserPlugin extends WFMediaManager
 
         // get the plugin that opened the file browser
         $caller = $this->get('caller', 'browser');
-        $filter = JRequest::getVar('filter', 'files');
+
+        // get mediatype from xml
+        $mediatype = JRequest::getVar('mediatype', 'files');
 
         // clean filter value
-        $filter = (string) preg_replace('/[^\w_,]/i', '', $filter);
+        $mediatype = (string) preg_replace('/[^\w_,]/i', '', $mediatype);
 
         // get filetypes from params
         $filetypes = $this->getParam('browser.extensions', $this->get('_filetypes'));
 
-        if ($filter == 'images') {
-            $filetypes = 'jpg,jpeg,png,gif';
-        } elseif ($filter === 'media') {
-            $filetypes = 'avi,wmv,wm,asf,asx,wmx,wvx,mov,qt,mpg,mpeg,m4a,swf,dcr,rm,ra,ram,divx,mp4,ogv,ogg,webm,flv,f4v,mp3,ogg,wav,xap';
-        } elseif ($filter === 'html') {
-            $filetypes = 'html,htm,txt';
-        } else if (strpos($filter, ',') !== false) {
-            $filetypes = $filter;
+        $map = array(
+            "images" => "jpg,jpeg,png,gif",
+            "media"  => "avi,wmv,wm,asf,asx,wmx,wvx,mov,qt,mpg,mpeg,m4a,m4v,swf,dcr,rm,ra,ram,divx,mp4,ogv,ogg,webm,flv,f4v,mp3,ogg,wav,xap",
+            "html"   => "html,htm,txt",
+            "files"  => $filetypes
+        );
+
+        // add svg support to images if it is allowed in filetypes
+        if (in_array('svg', explode(",", $filetypes))) {
+            $map['images'] = "jpg,jpeg,png,gif,svg";
+        }
+
+        if (array_key_exists($mediatype, $map)) {
+            $filetypes = $map[$mediatype];
+        } else {
+            $filetypes = $mediatype;
         }
 
         // set filetypes
