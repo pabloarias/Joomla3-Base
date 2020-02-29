345890a1c46189a66522219c27dd86bfe17a05a3
pabloarias <correo@pabloarias.eu>
2020-02-29 08:18:25 +0100

    Updated Joomla to 3.9.15

--- administrator/logs/joomla_update.php
+++ administrator/logs/joomla_update.php
@@ -12,3 +12,11 @@
 2019-11-27T17:38:03+00:00	INFO 127.0.0.1	update	Borrando los archivos y carpetas que han sido eliminados.
 2019-11-27T17:38:04+00:00	INFO 127.0.0.1	update	Limpieza posterior a la instalación.
 2019-11-27T17:38:04+00:00	INFO 127.0.0.1	update	La actualización a la versión 3.9.13 se ha completado.
+2020-02-29T07:15:44+00:00	INFO 127.0.0.1	update	Actualización inciciadoa por el usuario Super User (821). La versión antigua es la 3.9.13.
+2020-02-29T07:15:46+00:00	INFO 127.0.0.1	update	Descargando el archivo desde https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.15-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20200229%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200229T071529Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=0ddff8740c3431afbd2d3b149ecc4a0b4f477807562360014cbe420f8f4755af.
+2020-02-29T07:15:49+00:00	INFO 127.0.0.1	update	El archivoe Joomla_3.9.15-Stable-Update_Package.zip se ha descargado.
+2020-02-29T07:15:49+00:00	INFO 127.0.0.1	update	Iniciando la instalación de la versión nueva.
+2020-02-29T07:15:51+00:00	INFO 127.0.0.1	update	Finalizando la instalación.
+2020-02-29T07:15:51+00:00	INFO 127.0.0.1	update	Borrando los archivos y carpetas que han sido eliminados.
+2020-02-29T07:15:52+00:00	INFO 127.0.0.1	update	Limpieza posterior a la instalación.
+2020-02-29T07:15:52+00:00	INFO 127.0.0.1	update	La actualización a la versión 3.9.15 se ha completado.
