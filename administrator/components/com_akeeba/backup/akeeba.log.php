bcbbf621e1d2d372037aa7a142f053827a3f30af
pabloarias <correo@pabloarias.eu>
2020-05-30 00:40:11 +0200

    New JPA backup

---
+++ administrator/components/com_akeeba/backup/akeeba.log.php
@@ -0,0 +1,34 @@
+DEBUG   |200530 00:38:52| -- Loaded stored Akeeba Factory (backend) [backend]
+DEBUG   |200530 00:38:52|Fetching filter data from database
+DEBUG   |200530 00:38:52|Loading filters
+DEBUG   |200530 00:38:52|-- Loading filter Tables
+DEBUG   |200530 00:38:52|-- Loading filter Multidb
+DEBUG   |200530 00:38:52|-- Loading filter Regexskipdirs
+DEBUG   |200530 00:38:52|-- Loading filter Tabledata
+DEBUG   |200530 00:38:52|-- Loading filter Skipfiles
+DEBUG   |200530 00:38:52|-- Loading filter Regexskipfiles
+DEBUG   |200530 00:38:52|-- Loading filter Regexdirectories
+DEBUG   |200530 00:38:52|-- Loading filter Incremental
+DEBUG   |200530 00:38:52|-- Loading filter Files
+DEBUG   |200530 00:38:52|-- Loading filter Directories
+DEBUG   |200530 00:38:52|-- Loading filter Regextables
+DEBUG   |200530 00:38:52|-- Loading filter Regexfiles
+DEBUG   |200530 00:38:52|-- Loading filter Regextabledata
+DEBUG   |200530 00:38:52|-- Loading filter Extradirs
+DEBUG   |200530 00:38:52|-- Loading filter Skipdirs
+DEBUG   |200530 00:38:52|-- Loading filter Excludefiles
+DEBUG   |200530 00:38:52|-- Loading filter Sitedb
+DEBUG   |200530 00:38:52|-- Loading filter Libraries
+DEBUG   |200530 00:38:52|-- Loading filter Systemcachefiles
+DEBUG   |200530 00:38:52|-- Loading filter Excludetabledata
+DEBUG   |200530 00:38:52|-- Loading filter Excludefolders
+DEBUG   |200530 00:38:52|-- Loading filter Joomlaskipfiles
+DEBUG   |200530 00:38:52|-- Loading filter Joomlaskipdirs
+DEBUG   |200530 00:38:52|-- Loading filter Siteroot
+DEBUG   |200530 00:38:52|-- Loading filter Cvsfolders
+DEBUG   |200530 00:38:52|Loading optional filters
+DEBUG   |200530 00:38:52|-- Loading optional filter Stack\StackErrorlogs
+DEBUG   |200530 00:38:52|-- Loading optional filter Stack\StackHoststats
+DEBUG   |200530 00:38:52|-- Loading optional filter Stack\StackFinder
+DEBUG   |200530 00:38:52|-- Loading optional filter Stack\StackMyjoomla
+DEBUG   |200530 00:38:52|-- Loading optional filter Stack\StackActionlogs
